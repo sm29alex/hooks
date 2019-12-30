6d18593a449f5ceda2ff286fbbbb48e51e4cb9d5
sm29alex <sin7der>
2019-12-30 21:37:46 +0500

    qqq

--- src/index.js
+++ src/index.js
@@ -8,11 +8,26 @@ const App = () => <div>
 const HookSwitcher = () => {
 
     const [ color, setColor ] = useState('white')
+    const [ fontSize, setFontSize ] = useState(14);
 
     return (
-        <div style={{ padding: '10px', backgroundColor: color }}>
-            <button onClick={()=>setColor('black')}>Dark</button>
-            <button onClick={()=>setColor('white')}>Light</button>
+        <div style={{ padding: '10px', backgroundColor: color, fontSize: `${fontSize}px` }}>
+            <button 
+                onClick={()=>setColor('black')}>
+                Dark
+            </button>
+            <button 
+                onClick={()=>setColor('white')}>
+                Light
+            </button>
+            <button
+                onClick={() => setFontSize( (s) => s+1 )}>
+                Increment Size
+            </button>
+            <button
+                onClick={() => setFontSize( (s) => s-1 )}>
+                Decrement Size
+            </button>
         </div>
     )
 }
