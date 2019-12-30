d179418ce2e00eee2d3e67caae5b14943d56ad13
sm29alex <sm29alex@gmail.com>
2019-12-30 17:02:36 +0500

    dark-white

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <p>Hi</p>
+const App = () => <div>
+    <HookSwitcher />
+</div>
+
+const HookSwitcher = () => {
+
+    const [ color, setColor ] = useState('white')
+    return (
+        <div style={{ padding: '10px', backgroundColor: color }}>
+            <button onClick={()=>setColor('black')}>Dark</button>
+            <button onClick={()=>setColor('white')}>Light</button>
+        </div>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
